479f0cb5ba6687d6ec3d3f691c9df98660d399fa
rajthilakmca <rajthilak@megam.co.in>
2014-10-20 22:23:51 +0530

    Fix log file create and append logs

--- app/actions.go
+++ app/actions.go
@@ -1,42 +1,69 @@
 package app
 
 import (
-	"bytes"
 	"fmt"
 	"errors"
 	"github.com/megamsys/libgo/action"
 	"github.com/megamsys/libgo/exec"
 	log "code.google.com/p/log4go"
 	"strings"
+	"github.com/tsuru/config"
+	"os"
+	"path"
+	"bufio"
 	"github.com/megamsys/megamd/provisioner"
 )
 
-func CommandExecutor(app *provisioner.AssemblyResult) (action.Result, error) {
-	var e exec.OsExecutor
-	var b bytes.Buffer
-	
-	commandWords := strings.Fields(app.Command)
-	
-	fmt.Println(commandWords, len(commandWords))
-    fmt.Println("--------------------")
-    fmt.Println(commandWords[0])
-    fmt.Println(commandWords[4])
-    s := make([]string, 1, 25)
-    str := strings.Join(append(s[:2],commandWords...)," ")
-    fmt.Println("=========================")
-    fmt.Println(str)
-    s1 := make([]string, 2)
-    s1[0] = "-la"
-	if len(commandWords) > 0 {
-		//if err := e.Execute(commandWords[0], commandWords[1:len(commandWords)], nil, &b, &b); err != nil {
-		if err := e.Execute("ls", s1, nil, &b, &b); err != nil {
-			fmt.Println(err)
-			return nil, err
-		}
-	}
 
-	log.Info("%s", b)
-	return &app, nil
+func CommandExecutor(app *provisioner.AssemblyResult) (action.Result, error) {
+    var e exec.OsExecutor
+    var commandWords []string
+
+    commandWords = strings.Fields(app.Command)
+
+    megam_home, ckberr := config.GetString("MEGAM_HOME")
+	if ckberr != nil {
+		return nil, ckberr
+	}
+    appName := app.Name + "." + app.Components[0].Inputs.Domain
+	basePath := megam_home + "logs" 
+	dir := path.Join(basePath, appName)
+	fileOutPath := path.Join(dir, appName + "_out" )
+	fileErrPath := path.Join(dir, appName + "_err" )
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		fmt.Printf("no such file or directory: %s", dir)
+		if errm := os.MkdirAll(dir, 0777); errm != nil {
+			return nil, errm
+		}
+	} 
+		// open output file
+		fout, outerr := os.OpenFile(fileOutPath, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+		if outerr != nil {
+			return nil, outerr
+		}
+		defer fout.Close()
+		
+		fmt.Println(fout)
+		// open Error file
+		ferr, errerr := os.OpenFile(fileErrPath, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+		if errerr != nil {
+			return nil, errerr
+		}
+		defer ferr.Close()
+  
+	foutwriter := bufio.NewWriter(fout)
+	ferrwriter := bufio.NewWriter(ferr)
+    fmt.Println(commandWords, len(commandWords))
+    if len(commandWords) > 0 {
+       if err := e.Execute(commandWords[0], commandWords[1:], nil, foutwriter, ferrwriter); err != nil {
+           return nil, err
+        }
+     }
+
+   foutwriter.Flush()
+   ferrwriter.Flush()
+   
+  return &app, nil
 }
 
 
@@ -52,7 +79,7 @@ var launchedApp = action.Action{
 		default:
 			return nil, errors.New("First parameter must be App or *assemblies.AssemblyResult.")
 		}
-       
+		app.Command = "ls -la"
 		return CommandExecutor(&app)
 	},
 	Backward: func(ctx action.BWContext) {
