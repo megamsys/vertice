ab08f3d49ff4b3e8c29638b0bb0c969685ad79ca
Kishorekumar Neelamegam <nkishore@megam.co.in>
2014-10-27 16:33:41 +0530

    a minor fix to have single quoted json_attributes

--- iaas/ec2/iaas.go
+++ iaas/ec2/iaas.go
@@ -52,10 +52,7 @@ func (i *EC2IaaS) CreateMachine(pdc *iaas.PredefClouds, assembly *provisioner.As
         fmt.Println(aerr)
         return "", aerr
     }
-	str = str + " --json-attributes \'" + string(b) + "\'"
-	
-	//--json-attributes {"riak_host":"api.megam.co","accounts_id":"ACT1135880650419732480","assembly_id":"ASM1138425212728115200"}
-	
+	str = str + " --json-attributes " + `'` +string(b) + `'`
 	
 	//strings.Replace(str,"-c","-c "+assembly.Name+"."+assembly.Components[0].Inputs.Domain,-1)
 	knifePath, kerr := config.GetString("knife:path")
