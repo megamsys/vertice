9777549f4d48bf9a0932d1ce08e71ab602f46cc8
indykish <nkishore@megam.io>
2015-09-30 14:58:14 +0530

    route53 2 tests pass.

--- router/route53/router_test.go
+++ router/route53/router_test.go
@@ -1,13 +1,12 @@
 package route53
-/*
-import (
-	"fmt"
-	"net/http"
-	"net/url"
-	"testing"
-	"time"
 
+import (
+//	"fmt"
+	"testing"
+
+	"github.com/BurntSushi/toml"
 	"github.com/megamsys/megamd/router"
+	"github.com/megamsys/megamd/subd/dns"
 	"gopkg.in/check.v1"
 )
 
@@ -16,142 +15,82 @@ func Test(t *testing.T) {
 }
 
 type S struct {
-	c *dns.Config
+	cf *dns.Config
 }
 
 var _ = check.Suite(&S{})
 
 func (s *S) SetUpSuite(c *check.C) {
-	c := dns.NewConfig()
+	cf := dns.NewConfig()
 	if _, err := toml.Decode(`
 enabled = true
 access_key  = "temp_access_key"
-secrete_key = "temp_secret_key"
-`, &d); err != nil {
+secret_key = "temp_secret_key"
+`, &cf); err != nil {
 		c.Fatal(err)
 	}
-	s.c = c
+	cf.MkGlobal()
+	s.cf = cf
 }
-
 
 func (s *S) TestShouldBeRegistered(c *check.C) {
-	got, err := router.Get("vulcand")
+	router.Register("route53", createRouter)
+	got, err := router.Get("route53")
 	c.Assert(err, check.IsNil)
-	r, ok := got.(*vulcandRouter)
+	_, ok := got.(route53Router)
 	c.Assert(ok, check.Equals, true)
-	c.Assert(r.client.Addr, check.Equals, s.vulcandServer.URL)
-	c.Assert(r.domain, check.Equals, "vulcand.example.com")
 }
-
+/*
 func (s *S) TestSetCName(c *check.C) {
-	vRouter, err := router.Get("vulcand")
+	vRouter, err := router.Get("route53")
 	c.Assert(err, check.IsNil)
-	err = vRouter.AddBackend("myapp")
+	err = vRouter.SetCName("myapp1.megambox.com", "192.168.1.100")
 	c.Assert(err, check.IsNil)
-	u1, _ := url.Parse("http://1.1.1.1:111")
-	u2, _ := url.Parse("http://2.2.2.2:222")
-	err = vRouter.AddRoute("myapp", u1)
-	c.Assert(err, check.IsNil)
-	err = vRouter.AddRoute("myapp", u2)
-	c.Assert(err, check.IsNil)
-	err = vRouter.SetCName("myapp.cname.example.com", "myapp")
-	c.Assert(err, check.IsNil)
-	appFrontend, err := s.engine.GetFrontend(engine.FrontendKey{
-		Id: "tsuru_myapp.vulcand.example.com",
-	})
-	c.Assert(err, check.IsNil)
-	cnameFrontend, err := s.engine.GetFrontend(engine.FrontendKey{
-		Id: "tsuru_myapp.cname.example.com",
-	})
-	c.Assert(err, check.IsNil)
-	c.Assert(cnameFrontend.BackendId, check.DeepEquals, appFrontend.BackendId)
-	c.Assert(cnameFrontend.Route, check.Equals, `Host("myapp.cname.example.com")`)
-	c.Assert(cnameFrontend.Type, check.Equals, "http")
 }
 
 func (s *S) TestSetCNameDuplicate(c *check.C) {
-	vRouter, err := router.Get("vulcand")
-	c.Assert(err, check.IsNil)
-	err = vRouter.AddBackend("myapp")
-	c.Assert(err, check.IsNil)
-	u1, _ := url.Parse("http://1.1.1.1:111")
-	u2, _ := url.Parse("http://2.2.2.2:222")
-	err = vRouter.AddRoute("myapp", u1)
-	c.Assert(err, check.IsNil)
-	err = vRouter.AddRoute("myapp", u2)
-	c.Assert(err, check.IsNil)
-	err = vRouter.SetCName("myapp.cname.example.com", "myapp")
-	c.Assert(err, check.IsNil)
-	err = vRouter.SetCName("myapp.cname.example.com", "myapp")
+	vRouter, err := router.Get("route53")
+	err = vRouter.SetCName("myapp1.megambox.com", "192.168.1.100")
 	c.Assert(err, check.Equals, router.ErrCNameExists)
 }
 
 func (s *S) TestUnsetCName(c *check.C) {
-	vRouter, err := router.Get("vulcand")
+	vRouter, err := router.Get("route53")
 	c.Assert(err, check.IsNil)
-	err = vRouter.AddBackend("myapp")
+	err = vRouter.UnsetCName("myapp1.megambox.com", "192.168.1.100")
 	c.Assert(err, check.IsNil)
-	u1, _ := url.Parse("http://1.1.1.1:111")
-	u2, _ := url.Parse("http://2.2.2.2:222")
-	err = vRouter.AddRoute("myapp", u1)
-	c.Assert(err, check.IsNil)
-	err = vRouter.AddRoute("myapp", u2)
-	c.Assert(err, check.IsNil)
-	err = vRouter.SetCName("myapp.cname.example.com", "myapp")
-	c.Assert(err, check.IsNil)
-	frontends, err := s.engine.GetFrontends()
-	c.Assert(err, check.IsNil)
-	c.Assert(frontends, check.HasLen, 2)
-	vRouter.UnsetCName("myapp.cname.example.com", "myapp")
-	frontends, err = s.engine.GetFrontends()
-	c.Assert(err, check.IsNil)
-	c.Assert(frontends, check.HasLen, 1)
-	c.Assert(frontends[0].Id, check.Equals, "tsuru_myapp.vulcand.example.com")
 }
 
 func (s *S) TestUnsetCNameNotExist(c *check.C) {
-	vRouter, err := router.Get("vulcand")
+	vRouter, err := router.Get("route53")
 	c.Assert(err, check.IsNil)
-	frontends, err := s.engine.GetFrontends()
-	c.Assert(err, check.IsNil)
-	c.Assert(frontends, check.HasLen, 0)
-	err = vRouter.UnsetCName("myapp.cname.example.com", "myapp")
+	err = vRouter.UnsetCName("myapp2.megambox.com", "192.168.1.102")
 	c.Assert(err, check.Equals, router.ErrCNameNotFound)
 }
 
 func (s *S) TestAddr(c *check.C) {
-	vRouter, err := router.Get("vulcand")
+	vRouter, err := router.Get("route53")
 	c.Assert(err, check.IsNil)
-	err = vRouter.AddBackend("myapp")
+	addr, err := vRouter.Addr("myapp.megambox.com")
 	c.Assert(err, check.IsNil)
-	addr, err := vRouter.Addr("myapp")
-	c.Assert(err, check.IsNil)
-	c.Assert(addr, check.Equals, "myapp.vulcand.example.com")
+	c.Assert(addr, check.Equals, "megambox.com")
 }
 
 func (s *S) TestAddrNotExist(c *check.C) {
-	vRouter, err := router.Get("vulcand")
+	vRouter, err := router.Get("route53")
 	c.Assert(err, check.IsNil)
-	frontends, err := s.engine.GetFrontends()
-	c.Assert(err, check.IsNil)
-	c.Assert(frontends, check.HasLen, 0)
-	backends, err := s.engine.GetBackends()
-	c.Assert(err, check.IsNil)
-	c.Assert(backends, check.HasLen, 0)
-	addr, err := vRouter.Addr("myapp")
-	c.Assert(err, check.Equals, router.ErrBackendNotFound)
+	addr, err := vRouter.Addr("myapp.megambox.com")
+	//c.Assert(err, check.Equals, router.ErrBackendNotFound)
 	c.Assert(addr, check.Equals, "")
 }
 
 func (s *S) TestStartupMessage(c *check.C) {
-	got, err := router.Get("vulcand")
+	got, err := router.Get("route53")
 	c.Assert(err, check.IsNil)
 	mRouter, ok := got.(router.MessageRouter)
 	c.Assert(ok, check.Equals, true)
 	message, err := mRouter.StartupMessage()
 	c.Assert(err, check.IsNil)
-	c.Assert(message, check.Equals,
-		fmt.Sprintf(`vulcand router "vulcand.example.com" with API at "%s"`, s.vulcandServer.URL),
-	)
+	c.Assert(message, check.Equals, fmt.Sprintf("R53 router => ok!"))
 }
 */
