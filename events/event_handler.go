dce7993338bb931bdf1c1bb9bce56205757aed71
indykish <nkishore@megam.io>
2016-01-27 15:58:00 +0530

    added megamd.conf back

--- events/event_handler.go
+++ events/event_handler.go
@@ -1,7 +1,22 @@
 package events
 
-/*type EventChannelHandler  interface {
+// Interface for event  operation handlers.
+type EventHandler interface {
+	// Returns the ContainerReference
+	//VMReference() (info.ContainerReference, error) //*EventHoldingReference - can be an interface which get casted ?
 
-Handle(e Event) error
+	// Registers a channel to listen for events affecting vm(eventholder) (recursively).
+	Watch(events chan Event) error
 
-}*/
+	// Stops watching for changes.
+	StopWatching() error
+
+	// Returns whether the vm(type of eventholder) still exists.
+	Exists() bool
+
+	Cleanup()
+
+	// Start starts any necessary background goroutines - must be cleaned up in Cleanup().
+	// It is expected that most implementations will be a no-op.
+	Start()
+}
